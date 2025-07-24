b8c98d81302df86d872cb4c1be50109e4f544670
Mohamed Belaouer <mohamedbelaouer93@gmail.com>
2025-07-24 20:51:35 +0200

    créer le controller users & jwtauthguard

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getMe(@Request() req) {
+    return req.user;
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    const user = await this.usersService.findById(id);
+    if (user) {
+      const { password, ...rest } = user;
+      return rest;
+    }
+    return null;
+  }
+}
